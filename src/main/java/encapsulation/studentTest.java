ccf2c911fd908f13d2daa3987f21d4f45791d073
raj <raj@gmail.com>
2023-02-06 13:14:51 +0530

    only get the valur

--- src/main/java/encapsulation/studentTest.java
+++ src/main/java/encapsulation/studentTest.java
@@ -3,7 +3,7 @@ package encapsulation;
 public class studentTest {
     public static void main(String[] args) {
         Student s=new Student();
-        s.setName("raj");
+       // s.setName("raj");
 
         System.out.println(s.getName());
     }

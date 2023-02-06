4ba54cd426b524a51937f44623e0100ad2088d8e
raj <raj@gmail.com>
2023-02-06 12:59:50 +0530

    encapsulation

---
+++ src/main/java/encapsulation/Student.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Student {
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/encapsulation/studentTest.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class studentTest {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.setName("raj");
+
+        System.out.println(s.getName());
+    }
+}
